508ba90b367fec6b9d8487c7bf77fa00ad23dcac
Prakashparaak022 <prakashparaak@gmail.com>
2024-11-15 14:55:41 +0530

    build issues fixed

--- Frontend/pages/_app.js
+++ Frontend/pages/_app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Home from "./Home";
-import AboutSection from "./About";
+import AboutSection from "./about/index.js";
 import Services from "./services";
 import ApplyNowForm from "./apply";
 import ContactUsForm from "./Contact";
